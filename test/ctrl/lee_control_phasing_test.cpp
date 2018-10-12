45bb5a7df439dea7acfdc0e636c238b454e15c20
Gene Merewether <mereweth@jpl.nasa.gov>
2018-10-12 09:42:25 -0700

    Add shell for phasing test for lee controller

---
+++ test/ctrl/lee_control_phasing_test.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+#include "quest_gnc/ctrl/lee_control.h"
+
+TEST(SmallMagnitudeLinear, pos)
+{
+}
+
+TEST(SmallMagnitudeLinear, vel)
+{
+}
+
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
